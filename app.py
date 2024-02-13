2c9c415d4dab042691d6b1680fdf63c81ea0c5d8
kena-code <emmanuelkpadedji@gmail.com>
2024-02-13 19:40:37 +0300

    Backend Basic part

---
+++ app.py
@@ -0,0 +1,13 @@
+# using Python Flask to serve the frontend (profile page)Basic part
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
