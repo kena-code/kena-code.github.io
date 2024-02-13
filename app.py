861b3448fae87f7bd2c5a4a04621875930a383e1
kena-code <emmanuelkpadedji@gmail.com>
2024-02-13 19:45:07 +0300

    Backend advenced part

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-# using Python Flask to serve the frontend (profile page)Basic part
-
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
+# Define a default profile
+default_profile = {
+    'names': 'Emmanuel Noe A. KPADEDJI',
+    'age': 27,
+    'location': 'Saint Petersburg'
+}
+
+# Route for the default page, redirects to the profile page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('profile'))
+
+# Route for the profile page
+@app.route('/profile')
+def profile():
+    return render_template('index.html', **default_profile)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
